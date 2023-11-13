3370af89732104bea5332ad58d8b06b89cfdaa9a
Hema Urao <hemauraw033@gmail.com>
2023-11-13 04:14:45 -0800

    first commit

---
+++ cal.py
@@ -0,0 +1,18 @@
+no1=int(input("enter no. :-"))
+opr=input("enter oprator:-")
+no2=int(input("enter no. :-"))
+if opr=='+':
+    print(no1+no2)
+
+elif opr=='-':
+    print(no1-no2)
+
+elif opr=='*':
+    print(no1*no2)
+
+elif opr=='/':       
+    print(no1/no2)
+
+else:
+    print("invalid") 
+       
